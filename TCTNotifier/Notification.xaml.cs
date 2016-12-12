9c34dcf53dc3aad903d6def11d10fe0772e179d3
foglio <foglio1024@gmail.com>
2016-12-12 16:40:56 +0100

    made notification transparent to click

--- TCTNotifier/Notification.xaml.cs
+++ TCTNotifier/Notification.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
@@ -25,7 +27,12 @@ namespace TCTNotifier
             InitializeComponent();
             
         }
-
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var hwnd = new WindowInteropHelper(this).Handle;
+            WindowsServices.SetWindowExTransparent(hwnd);
+        }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
@@ -60,4 +67,5 @@ namespace TCTNotifier
         }
 
     }
+
 }
